323cd1147919b16abe2083838a93b9fb348d36fd
Ayko Sousa do Nascimento <ayko.souza74@gmail.com>
2024-08-03 12:10:48 -0300

    feat: save user on database and check if users exisits

--- app.js
+++ app.js
@@ -8,10 +8,69 @@ const app = express();
 // Config JSON response
 app.use(express.json());
 
+// Models
+const User = require('./models/User');
+
 app.post('/auth/register', async(req, res) => {
     const {name, email, password, confirmpassword} = req.body;
     if(!name) {
         return res.status(400).json({message: 'Name is required'});
+    }
+    if(!email) {
+        return res.status(400).json({message: 'Email is required'});
+    }
+    if(!password) {
+        return res.status(400).json({message: 'Password is required'});
+    }
+    if(!confirmpassword) {
+        return res.status(400).json({message: 'Password confirmation is required'});
+    }
+    if(password !== confirmpassword) {
+        return res.status(400).json({message: 'Passwords do not match'});
+    }
+
+    const userExistis = await User.findOne({ email: email});
+    if(userExistis) {
+        return res.status(400).json({message: 'Email already exists'});
+    }
+
+    const salt = await bcrypt.genSalt(12);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const user = new User({
+        name,
+        email,
+        password: passwordHash
+    });
+
+    try {
+        await user.save();
+        res.status(201).json({message: "User created succefully!"})
+
+    } catch (err) {
+        res.status(500).json({message: "Internal server error!"});
+        console.error(err);
+    }
+});
+
+app.post('/auth/login', async(req, res) => {
+    const {email, password} = req.body;
+    if(!email) {
+        return res.status(400).json({message: 'Email is required'});
+    }
+    if(!password) {
+        return res.status(400).json({message: 'Password is required'});
+    }
+
+    const user = await User.findOne({ email: email });
+
+    if(!user) {
+        return res.status(404).json({message: 'Email does not exist'});
+    }
+
+    const checkPassword = await bcrypt.compare(password, user.password);
+    if(!checkPassword) {
+        return res.status(400).json({message: 'Invalid password!'});
     }
 });
 
