f7be7fa71da60e1dee594cd18b67dcad3340541d
Ayko Sousa do Nascimento <ayko.souza74@gmail.com>
2024-08-03 11:26:21 -0300

    config: connect to mongodb

--- app.js
+++ app.js
@@ -5,4 +5,14 @@ const jwt = require('jsonwebtoken');
 
 const app = express();
 
-app.listen(3000);
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.yk8n8tr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+    .then(() => {
+        app.listen(3000);
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+    console.log(err);
+})
